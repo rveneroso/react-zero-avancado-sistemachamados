3a26178fee12d12952ba32064a729cc61de03004
Renato Veneroso <rveneroso@gmail.com>
2024-09-23 19:27:47 -0300

    Código da aula 98. Criadas as rotas iniciais (login e cadastro).

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
   );
 }
 
