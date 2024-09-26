64b7f86a7436aba42c1816a77876af571539d29d
Renato Veneroso <rveneroso@gmail.com>
2024-09-25 21:19:35 -0300

    Código da aula 100 - Criação das páginas de cadastro e dashboard (ainda não funcionais).

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,47 @@
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
 export default function SignUp() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
-        <div>
-            <h1>Página Cadastro</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt="Logo do sistema de chamados" />
+                </div>
+
+                <form>
+                    <h1>Nova conta</h1>
+                    <input
+                        type='text'
+                        placeholder='Seu nome'
+                        value={useState.name}
+                        onChange={ (e) => setName(e.target.value)}
+                    />
+
+                    <input
+                    type='text'
+                    placeholder='email@email.com'
+                    value={useState.email}
+                    onChange={ (e) => setEmail(e.target.value)}
+                    />
+
+                    <input
+                        type='password'
+                        placeholder='******'
+                        value={useState.password}
+                        onChange={ (e) => setPassword(e.target.value)}
+                    />
+
+                    <button type='submit'>Cadastrar</button>
+                </form>
+
+                <Link to="/">Já possui uma conta? Faça login</Link>
+            </div>
         </div>
     )
 }
