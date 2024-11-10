cc20ea8b7b171e6a74103d60b287608f221ef959
Renato Veneroso <rveneroso@gmail.com>
2024-11-10 12:25:23 -0300

    Código da aula 106 - Criando páginas privadas

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
 import { auth, db } from '../services/firebaseConnection';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 import { useNavigate } from 'react-router-dom';
@@ -12,8 +12,23 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }) {
     const [user,setUser] = useState(null);
     const [loadingAuth, setLoadingAuth] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        async function loadUser() {
+            const storageUser = localStorage.getItem("@ticketsPRO");
+
+            if(storageUser) {
+                setUser(JSON.parse(storageUser));
+                setLoading(false);
+            }
+            setLoading(false);
+        }
+
+        loadUser();
+    }, [])
 
     async function signIn(email, password) {
         setLoadingAuth(true);
@@ -80,6 +95,12 @@ function AuthProvider({ children }) {
         localStorage.setItem("@ticketsPRO", JSON.stringify(data));
     }
 
+    async function logout() {
+        await signOut(auth);
+        localStorage.removeItem("@ticketsPRO");
+        setUser(null);
+    }
+
     return(
         <AuthContext.Provider
             value={{
@@ -87,7 +108,9 @@ function AuthProvider({ children }) {
                 user,
                 signIn,
                 signUp,
-                loadingAuth
+                logout,
+                loadingAuth,
+                loading
             }}>
             {children}
         </AuthContext.Provider>
