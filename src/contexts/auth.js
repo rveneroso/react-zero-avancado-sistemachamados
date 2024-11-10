bb5b8c8d6e89e176e456169715bf290c66fafa80
Renato Veneroso <rveneroso@gmail.com>
2024-11-10 11:12:54 -0300

    Código da aula 105 - Logando um usuário

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
 import { auth, db } from '../services/firebaseConnection';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 import { useNavigate } from 'react-router-dom';
@@ -15,10 +15,34 @@ function AuthProvider({ children }) {
 
     const navigate = useNavigate();
 
-    function signIn(email, password) {
-        console.log(email);
-        console.log(password);
-        alert('LOGADO COM SUCESSO');
+    async function signIn(email, password) {
+        setLoadingAuth(true);
+
+        await signInWithEmailAndPassword(auth, email, password)
+        .then(async (value) => {
+            let uid = value.user.uid;
+
+            const docRef = doc(db, "users", uid);
+            const docSnap = await getDoc(docRef);
+
+            let data = {
+                uid: uid,
+                nome: docSnap.data().nome,
+                email: value.user.email,
+                avatarUrl: docSnap.data().avatarUrl
+            }
+
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+            toast.success(`Bem-vindo(a) de volta ${data.nome} !`)
+            navigate("/dashboard");
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoadingAuth(false);
+            toast.error("Oops!!! Algo deu errado!");
+        })
     }
 
     async function signUp(email, password, name) {
